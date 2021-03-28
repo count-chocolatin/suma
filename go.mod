183b49d9eaac5331dc959fec67d8235b1170dd21
lordpandita <78991960+nombregithub@users.noreply.github.com>
2021-03-28 01:30:43 -0500

    module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/count-chocolatin/suma
+
+go 1.16
---
+++ suma.go
@@ -0,0 +1,11 @@
+package suma
+
+func Plus(a int, b int) int {
+
+    return a + b
+}
+
+func PlusPlus(a, b, c int) int {
+    return a + b + c
+}
+
